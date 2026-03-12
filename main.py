464c326bc104095b01eea0c9c9a1fb2ffab02f39
kivancakmak <93039268+kivancakmak@users.noreply.github.com>
2026-03-12 23:48:10 +0300

    Last updates

--- main.py
+++ main.py
@@ -3,11 +3,7 @@ from custom_classes import Calculator
 calc = Calculator()
 print(calc._current_val)  # Accessing the private attribute (not recommended)
 
-# Ödevdeki örnek: (10 + 5) * 2 / 3
-try:
-    step1 = calc.add(10, 5)
-    step2 = calc.multiply(step1, 2)
-    result = calc.divide(step2, 3)
-    print(f"Final Result: {result}")
-except ValueError as e:
-    print(e)
+#(10 + 5) * 2 / 3
+result = calc.divide(calc.multiply(calc.add(10,5),2),3)
+
+print(result)
