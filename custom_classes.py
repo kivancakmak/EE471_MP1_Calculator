a2c078acc73c077539213cd01f134a9c400b57f5
kivancakmak <kivanccakmak35@gmail.com>
2026-03-12 23:13:42 +0300

    saving local files before merging

---
+++ custom_classes.py
@@ -0,0 +1,14 @@
+class Calculator:
+    def __init__(self):
+        self._current_val = 0 # Private attribute to store the current value
+        
+    def add(self, x, y):
+    result = x + y
+    self._current_val = result # update_current_val 
+    return result
+
+    def subtract(self, x, y):
+    result = x - y
+    self._current_val = result
+    return result
+
