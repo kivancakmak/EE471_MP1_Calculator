92089648dc75dd28182c3719d0760100abd4039d
kivancakmak <kivanccakmak35@gmail.com>
2026-03-12 22:45:29 +0300

    Division method implemented

--- custom_classes.py
+++ custom_classes.py
@@ -2,3 +2,9 @@ class Calculator:
     def __init__(self):
         self._current_val = 0 # Private attribute to store the current value
         
+    def divide(self, x, y):
+    if y == 0:
+        raise ValueError("Division by zero is not allowed!") # Constraint
+    result = x / y
+    self._current_val = result
+    return result
