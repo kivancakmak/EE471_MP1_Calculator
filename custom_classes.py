e3aafb024e1b93e1917562c9577c06922983d49c
kivancakmak <kivanccakmak35@gmail.com>
2026-03-12 22:35:48 +0300

    Add method implemented

--- custom_classes.py
+++ custom_classes.py
@@ -2,3 +2,7 @@ class Calculator:
     def __init__(self):
         self._current_val = 0 # Private attribute to store the current value
         
+    def add(self, x, y):
+    result = x + y
+    self.__current_val = result # update_current_val 
+    return result
